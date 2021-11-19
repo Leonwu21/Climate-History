d385c6bec930e477677bc5a19665c84b99a9df87
Leon Wu <21leonwu@gmail.com>
2021-11-18 21:27:26 -0800

    Implemented AnyChart with Global Temperature data parsed from text file. Also added horizontal layout file

--- app/src/main/java/ca/bcit/climate_history/GlobalTemperature.java
+++ app/src/main/java/ca/bcit/climate_history/GlobalTemperature.java
@@ -17,72 +17,84 @@ import com.anychart.enums.MarkerType;
 import com.anychart.enums.TooltipPositionMode;
 import com.anychart.graphics.vector.Stroke;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class GlobalTemperature extends AppCompatActivity {
 
+    /**
+     * Reads in file, parses the information, and returns it in a list of DataEntry objects
+     * @param filename name of text file to read
+     * @param data list of DataEntry objects
+     * @return list of DataEntry objects
+     * @throws IOException
+     */
+    public List<DataEntry> readFile(String filename, List<DataEntry> data) throws IOException {
+        ArrayList<Double> avg = new ArrayList<Double>();
+        ArrayList<Double> lowess = new ArrayList<Double>();
+        ArrayList<String> years = new ArrayList<String>();
+
+        InputStream is = null;
+        try {
+            is = getAssets().open(filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            String [] nums = line.split(";");
+            years.add(nums[0]);
+            avg.add(Double.parseDouble(nums[1]));
+            lowess.add(Double.parseDouble(nums[2]));
+        }
+        for (int i = 0; i < avg.size(); i++) {
+            data.add(new CustomDataEntry(years.get(i), avg.get(i), lowess.get(i)));
+        }
+        return data;
+    }
+
+    /**
+     * Creates the line graph (AnyChart) upon Activity construction.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_global_temperature);
-        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
-        //anyChartView.setProgressBar(findViewById(R.id.progress_bar));
+        AnyChartView anyChartView = findViewById(R.id.gt_graph);
 
         Cartesian cartesian = AnyChart.line();
-
         cartesian.animation(true);
-
         cartesian.padding(10d, 20d, 5d, 20d);
-
         cartesian.crosshair().enabled(true);
         cartesian.crosshair()
                 .yLabel(true)
-                // TODO ystroke
                 .yStroke((Stroke) null, null, null, (String) null, (String) null);
 
         cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
-
-        cartesian.title("Trend of Sales of the Most Popular Products of ACME Corp.");
-
-        cartesian.yAxis(0).title("Number of Bottles Sold (thousands)");
+        cartesian.title("Land-Ocean Temperature Index (C)");
+        cartesian.yAxis(0).title("Temperature Anomaly (C)");
         cartesian.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
 
         List<DataEntry> seriesData = new ArrayList<>();
-        seriesData.add(new CustomDataEntry("1986", 3.6, 2.3, 2.8));
-        seriesData.add(new CustomDataEntry("1987", 7.1, 4.0, 4.1));
-        seriesData.add(new CustomDataEntry("1988", 8.5, 6.2, 5.1));
-        seriesData.add(new CustomDataEntry("1989", 9.2, 11.8, 6.5));
-        seriesData.add(new CustomDataEntry("1990", 10.1, 13.0, 12.5));
-        seriesData.add(new CustomDataEntry("1991", 11.6, 13.9, 18.0));
-        seriesData.add(new CustomDataEntry("1992", 16.4, 18.0, 21.0));
-        seriesData.add(new CustomDataEntry("1993", 18.0, 23.3, 20.3));
-        seriesData.add(new CustomDataEntry("1994", 13.2, 24.7, 19.2));
-        seriesData.add(new CustomDataEntry("1995", 12.0, 18.0, 14.4));
-        seriesData.add(new CustomDataEntry("1996", 3.2, 15.1, 9.2));
-        seriesData.add(new CustomDataEntry("1997", 4.1, 11.3, 5.9));
-        seriesData.add(new CustomDataEntry("1998", 6.3, 14.2, 5.2));
-        seriesData.add(new CustomDataEntry("1999", 9.4, 13.7, 4.7));
-        seriesData.add(new CustomDataEntry("2000", 11.5, 9.9, 4.2));
-        seriesData.add(new CustomDataEntry("2001", 13.5, 12.1, 1.2));
-        seriesData.add(new CustomDataEntry("2002", 14.8, 13.5, 5.4));
-        seriesData.add(new CustomDataEntry("2003", 16.6, 15.1, 6.3));
-        seriesData.add(new CustomDataEntry("2004", 18.1, 17.9, 8.9));
-        seriesData.add(new CustomDataEntry("2005", 17.0, 18.9, 10.1));
-        seriesData.add(new CustomDataEntry("2006", 16.6, 20.3, 11.5));
-        seriesData.add(new CustomDataEntry("2007", 14.1, 20.7, 12.2));
-        seriesData.add(new CustomDataEntry("2008", 15.7, 21.6, 10));
-        seriesData.add(new CustomDataEntry("2009", 12.0, 22.5, 8.9));
+        try {
+            readFile("GlobalTemperature.txt", seriesData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         Set set = Set.instantiate();
         set.data(seriesData);
         Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
         Mapping series2Mapping = set.mapAs("{ x: 'x', value: 'value2' }");
-        Mapping series3Mapping = set.mapAs("{ x: 'x', value: 'value3' }");
 
         Line series1 = cartesian.line(series1Mapping);
-        series1.name("Brandy");
+        series1.name("Average");
         series1.hovered().markers().enabled(true);
         series1.hovered().markers()
                 .type(MarkerType.CIRCLE)
@@ -94,24 +106,13 @@ public class GlobalTemperature extends AppCompatActivity {
                 .offsetY(5d);
 
         Line series2 = cartesian.line(series2Mapping);
-        series2.name("Whiskey");
+        series2.name("Lowess Smoothing");
+        series2.stroke("2 red");
         series2.hovered().markers().enabled(true);
         series2.hovered().markers()
                 .type(MarkerType.CIRCLE)
                 .size(4d);
         series2.tooltip()
-                .position("right")
-                .anchor(Anchor.LEFT_CENTER)
-                .offsetX(5d)
-                .offsetY(5d);
-
-        Line series3 = cartesian.line(series3Mapping);
-        series3.name("Tequila");
-        series3.hovered().markers().enabled(true);
-        series3.hovered().markers()
-                .type(MarkerType.CIRCLE)
-                .size(4d);
-        series3.tooltip()
                 .position("right")
                 .anchor(Anchor.LEFT_CENTER)
                 .offsetX(5d)
@@ -124,12 +125,12 @@ public class GlobalTemperature extends AppCompatActivity {
         anyChartView.setChart(cartesian);
     }
 
+    // Custom DataEntry object for graph
     private class CustomDataEntry extends ValueDataEntry {
 
-        CustomDataEntry(String x, Number value, Number value2, Number value3) {
+        CustomDataEntry(String x, Number value, Number value2) {
             super(x, value);
             setValue("value2", value2);
-            setValue("value3", value3);
         }
 
     }
