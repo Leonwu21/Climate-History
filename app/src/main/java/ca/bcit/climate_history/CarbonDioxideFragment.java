0dc31a3787510c3b6640519b849de2cdcc5599ea
bhalim1994 <bhalim1994@gmail.com>
2021-11-27 12:56:00 -0800

    Backup before deleting all fragments

---
+++ app/src/main/java/ca/bcit/climate_history/CarbonDioxideFragment.java
@@ -0,0 +1,17 @@
+package ca.bcit.climate_history;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class CarbonDioxideFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_carbon_dioxide, container, false);
+    }
+}
